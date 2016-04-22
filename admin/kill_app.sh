9113e730d31d7d042dd8388ded5bb82d55a5494d
Wesley <milo974@gmail.com>
2016-04-22 19:54:20 +0400

    remove old .sh

--- admin/kill_app.sh
+++
@@ -1,1 +0,0 @@
-kill $(pgrep -f mongrel) 2> /dev/null
--- admin/run_app.sh
+++
@@ -1,3 +0,0 @@
-DEFAULT=/var/mailserver
-rm -rf $DEFAULT/admin/{log,tmp}/* 2>/dev/null
-env LD_PRELOAD=/usr/local/lib/ruby/gems/1.8/gems/mysql-2.9.1/lib/mysql/mysql_api.so /usr/local/bin/mongrel_rails start -c $DEFAULT/admin -p 4213 -a 127.0.0.1 -d -e production -P $DEFAULT/admin/log/mongrel.pid 2> /dev/null
