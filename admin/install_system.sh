21a170c81fda15371fdb32e19ce9f5f6c741217f
Wesley <milo974@gmail.com>
2016-04-25 15:37:25 +0400

    system config

--- admin/install_system.sh
+++ admin/install_system.sh
@@ -14,6 +14,8 @@ fi
 
 DEFAULT=/var/mailserver
 
+function already {
+
 mkdir -p /var/db/spamassassin
 
 pkg_add roundcubemail clamav postfix-3.0.3p0-mysql p5-Mail-SpamAssassin \
@@ -29,3 +31,45 @@ echo " -- Stop and disable unwanted services"
 echo " -- Set Ntpd"
 /usr/sbin/rcctl set ntpd flags -s
 /usr/sbin/rcctl restart ntpd
+
+echo " -- Set Postfix"
+/usr/local/sbin/postfix-enable
+mkdir -p /etc/postfix/sql
+install -m 644 $DEFAULT/admin/config/system/postfix/* /etc/postfix
+install -m 644 $DEFAULT/admin/config/system/postfix-sql/* /etc/postfix/sql
+/usr/sbin/rcctl enable postfix
+/usr/sbin/rcctl start postfix
+
+echo " -- Set Spamassassin"
+install -m 644 $DEFAULT/admin/config/system/spamassassin/* /etc/mail/spamassassin/local.cf
+/usr/sbin/rcctl enable spamassassin
+/usr/sbin/rcctl start spamassassin
+/usr/local/bin/sa-update -v
+
+echo " -- Set ClamAV"
+install -m 644 $DEFAULT/admin/config/system/clamav/*clam* /etc 2> /dev/null
+if [ ! -f /var/db/clamav/main.cld ]; then
+touch /var/log/clamd.log 2> /dev/null
+chown _clamav:_clamav /var/log/clamd.log
+touch /var/log/clam-update.log 2> /dev/null
+chown _clamav:_clamav /var/log/clam-update.log
+touch /var/log/freshclam.log 2> /dev/null
+chown _clamav:_clamav /var/log/freshclam.log
+mkdir -p /var/db/clamav
+chown -R _clamav:_clamav /var/db/clamav
+/usr/local/bin/freshclam --no-warnings
+fi
+/usr/sbin/rcctl enable freshclam
+/usr/sbin/rcctl enable clamd
+/usr/sbin/rcctl enable clamav_milter
+/usr/sbin/rcctl start freshclam
+/usr/sbin/rcctl start clamd
+/usr/sbin/rcctl start clamav_milter
+}
+echo " -- Set Dovecot"
+install -m 644 $DEFAULT/admin/config/system/dovecot/* /etc/dovecot
+touch /var/log/imap 2> /dev/null
+chgrp _dovecot /usr/local/libexec/dovecot/dovecot-lda
+chmod 4750 /usr/local/libexec/dovecot/dovecot-lda
+/usr/sbin/rcctl enable dovecot
+/usr/sbin/rcctl start dovecot
