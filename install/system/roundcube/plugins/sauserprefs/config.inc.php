1ca9da4a1bcd665881ba4f6a1980f17857a52265
Wesley <milo974@gmail.com>
2016-05-04 16:44:44 +0400

    set sauserprefs plugin

--- install/system/roundcube/plugins/sauserprefs/config.inc.php
+++ install/system/roundcube/plugins/sauserprefs/config.inc.php
@@ -1,72 +1,79 @@
 <?php
 
-/*
- +-----------------------------------------------------------------------+
- | SAUserPrefs configuration file                                        |
- |                                                                       |
- | This file is part of the RoundCube Webmail client                     |
- | Copyright (C) 2005-2009, RoundCube Dev. - Switzerland                 |
- | Licensed under the GNU GPL                                            |
- |                                                                       |
- +-----------------------------------------------------------------------+
-
-*/
+/**
+ * SAUserPrefs configuration file
+ */
 
 // spamassassin database settings
-$rcmail_config['sauserprefs_db_dsnw'] = 'mysql://mailadmin:mailadmin@localhost/mail';
+$config['sauserprefs_db_dsnw'] = 'mysql://mailadmin:mailadmin@localhost/mail';
 
 // PEAR database DSN for read only operations (if empty write database will be used)
 // useful for database replication
-$rcmail_config['sauserprefs_db_dsnr'] = '';
+$config['sauserprefs_db_dsnr'] = '';
 
 // use persistent db-connections
 // beware this will not "always" work as expected
 // see: http://www.php.net/manual/en/features.persistent-connections.php
-$rcmail_config['sauserprefs_db_persistent'] = FALSE;
+$config['sauserprefs_db_persistent'] = FALSE;
 
 // table that holds user prefs
-$rcmail_config['sauserprefs_sql_table_name'] = "userpref";
+$config['sauserprefs_sql_table_name'] = "userpref";
 
 // name of the username field in the user prefs table
-$rcmail_config['sauserprefs_sql_username_field'] = "username";
+$config['sauserprefs_sql_username_field'] = "username";
 
 // name of the preference field in the user prefs table, holds the name of the preference
-$rcmail_config['sauserprefs_sql_preference_field'] = "preference";
+$config['sauserprefs_sql_preference_field'] = "preference";
 
 // name of the value field in the user prefs table, holds the value of the preference
-$rcmail_config['sauserprefs_sql_value_field'] = "value";
+$config['sauserprefs_sql_value_field'] = "value";
+
+// username of the current user in the database, normaly %u (username from the session info)
+//      %u is replaced with the username (from the session info)
+//      %l is replaced with the local part of the username (if the username is an email address)
+//      %d is replaced with the domain part of the username (if the username is an email address or default mail domain if not)
+//      %i is replaced with the email address from the user's default identity
+$config['sauserprefs_userid'] = "%u";
 
 // username of the "global" or default settings user in the database, normaly $GLOBAL or @GLOBAL
-$rcmail_config['sauserprefs_global_userid'] = "@GLOBAL";
+$config['sauserprefs_global_userid'] = "\$GLOBAL";
 
-// enable the whitelists synchronisation, check README for more information
-$rcmail_config['sauserprefs_whitelist_sync'] = FALSE;
+// enable address whitelist synchronisation with address book(s)
+// possible values:
+//   false - synchronisation disabled
+//   true - synchronise with default address book
+//   array - an array of address book ids to synchronise with e.g. array('0', 'global'); '0' = the default address book
+$config['sauserprefs_abook_sync'] = FALSE;
 
-// id of the address book to synchronise the whitelist with, null for default RoundCube address book
-$rcmail_config['sauserprefs_whitelist_abook_id'] = null;
+// set to false to disable address book import
+// possible values:
+//   false - address import disabled
+//   true - import addresses from default address book
+//   array - an array of address book ids to import e.g. array('0', 'global'); '0' = the default address book
+$config['sauserprefs_abook_import'] = FALSE;
 
 // don't allow these settings to be overriden by the user
-// eg. $rcmail_config['sauserprefs_dont_override'] = array('required_score','rewrite_header Subject');
+// eg. $config['sauserprefs_dont_override'] = array('required_score','rewrite_header Subject');
 // to disable entire sections enter the secion name surrounded by braces. Sections are: general,tests,bayes,headers,report,addresses
-// eg. $rcmail_config['sauserprefs_dont_override'] = array('{tests}');
-$rcmail_config['sauserprefs_dont_override'] = array('{bayes}', '{tests}', '{headers}');
+// eg. $config['sauserprefs_dont_override'] = array('{tests}');
+$config['sauserprefs_dont_override'] = array();
 
 // default settings
 // these are overridden by $GLOBAL and user settings from the database
-$rcmail_config['sauserprefs_default_prefs'] = array(
+$config['sauserprefs_default_prefs'] = array(
 									"required_score" => "5",
-									"rewrite_header Subject" => "[SPAM _SCORE_]",
+									"rewrite_header Subject" => "*****SPAM*****",
 									"ok_languages" => "all",
 									"ok_locales" => "all",
 									"fold_headers" => "1",
 									"add_header all Level" => "_STARS(*)_",
 									"use_razor1" => "0",
-									"use_razor2" => "0",
-									"use_pyzor" => "0",
-									"use_dcc" => "0",
+									"use_razor2" => "1",
+									"use_pyzor" => "1",
+									"use_dcc" => "1",
 									"use_bayes" => "1",
 									"skip_rbl_checks" => "0",
-									"report_safe" => "0",
+									"report_safe" => "1",
 									"bayes_auto_learn" => "1",
 									"bayes_auto_learn_threshold_nonspam" => "0.1",
 									"bayes_auto_learn_threshold_spam" => "12.0",
@@ -74,26 +81,22 @@ $rcmail_config['sauserprefs_default_prefs'] = array(
 								);
 
 // spam score increment - increment values in the the score threshold drop down by this, from 0 to 10
-$rcmail_config['sauserprefs_score_inc'] = 1;
+$config['sauserprefs_score_inc'] = 1;
 
 // delete user bayesian data stored in database
 // the query can contain the following macros that will be expanded as follows:
-//      %u is replaced with the username (from the session info)
+//      %u is replaced with the username from the sauserprefs_userid setting above
 // use an array to run multiple queries
-// eg. $rcmail_config['sauserprefs_bayes_delete_query'] = array(
+// set to null to disable this option
+// eg. $config['sauserprefs_bayes_delete_query'] = array(
 //												'DELETE FROM bayes_seen WHERE id IN (SELECT id FROM bayes_vars WHERE username = %u);',
 //												'DELETE FROM bayes_token WHERE id IN (SELECT id FROM bayes_vars WHERE username = %u);',
 //												'DELETE FROM bayes_vars WHERE username = %u;',
 //												);
-$rcmail_config['sauserprefs_bayes_delete_query'] = '';
-
-// Override old or deprecated preferences: 'old name' => 'new name'
-$rcmail_config['sauserprefs_deprecated_prefs'] = array(
-												'required_hits' => 'required_score'
-											);
+$config['sauserprefs_bayes_delete_query'] = null;
 
 // define languages
-$rcmail_config['sauserprefs_languages'] = array(
+$config['sauserprefs_languages'] = array(
 									"af" => "Afrikaans",
 									"sq" => "Albanian",
 									"am" => "Amharic",
@@ -159,4 +162,3 @@ $rcmail_config['sauserprefs_languages'] = array(
 									"cy" => "Welsh",
 									"yi" => "Yiddish"
 								);
-?>
