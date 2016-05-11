ca81f8016f9287f451260d676a8189d4eac9d336
Charlie Root <root@openbsd.aise.local>
2016-05-11 15:26:16 +0400

    fix build database script

--- install/build_db.sh
+++ install/build_db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DEFAULT=/var/mailserver
-cd $DEFAULT/admin && env LD_PRELOAD=/usr/local/lib/ruby/gems/1.8/gems/mysql-2.9.1/lib/mysql/mysql_api.so rake -s db:setup RAIL_ENV=production
 /usr/local/bin/mysql -e "grant select on mail.* to 'postfix'@'localhost' identified by 'postfix';"
 /usr/local/bin/mysql -e "grant all privileges on mail.* to 'mailadmin'@'localhost' identified by 'mailadmin';"
+cd $DEFAULT/admin && env LD_PRELOAD=/usr/local/lib/ruby/gems/1.8/gems/mysql-2.9.1/lib/mysql/mysql_api.so rake -s db:setup RAIL_ENV=production
