0ba0de1d63d0e6ffc0cbd2771dc1ea4d85be11d4
Wesley <milo974@gmail.com>
2016-05-18 09:38:29 +0400

    sysmail, a script to enable logs for the first admin

---
+++ install/sysmail.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ALIASES=/etc/mail/aliases
+ROOT=/var/mailserver
+ADM=$(/usr/local/bin/mysql -u root < $ROOT/install/system/aliases/req.sql | grep -v ^id | awk '{print $4}')
+
+grep ^root $ALIASES > /dev/null
+if [ "$?" == 0 ]; then
+	grep -v ^root  $ALIASES > /tmp/aliases.tmp
+	mv /tmp/aliases.tmp $ALIASES
+	echo "root: $ADM" >> /etc/mail/aliases
+	/usr/bin/newaliases
+else
+	echo "root: $ADM" >> /etc/mail/aliases
+	/usr/bin/newaliases
+fi
---
+++ install/system/aliases/req.sql
@@ -0,0 +1,2 @@
+use mail;
+select * from admins;
