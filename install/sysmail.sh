6baac7d9e0286e7def1bde0e396e5423b7e8c10b
Wesley <milo974@gmail.com>
2016-05-19 10:25:09 +0400

    detect no email

--- install/sysmail.sh
+++ install/sysmail.sh
@@ -3,6 +3,10 @@ ALIASES=/etc/mail/aliases
 ROOT=/var/mailserver
 ADM=$(/usr/local/bin/mysql -u root < $ROOT/install/system/aliases/req.sql | grep -v ^id | awk '{print $4}')
 
+if [ -z "$ADM" ]; then
+	exit 1
+fi
+
 grep ^root $ALIASES > /dev/null
 if [ "$?" == 0 ]; then
 	grep -v ^root  $ALIASES > /tmp/aliases.tmp
