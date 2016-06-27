2c473bc4c07e461cd86f54495ae80e40f4d10db2
Wesley <milo974@gmail.com>
2016-06-27 16:31:47 +0400

    improve build_db.sh

--- install/build_db.sh
+++ install/build_db.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 DEFAULT=/var/mailserver
+
+_err() {
+echo "!!! ${@}"
+exit 1
+}
+
 /usr/local/bin/mysql -e "grant select on mail.* to 'postfix'@'localhost' identified by 'postfix';"
 /usr/local/bin/mysql -e "grant all privileges on mail.* to 'mailadmin'@'localhost' identified by 'mailadmin';"
+
+/usr/local/bin/mysql -e "show databases;" | grep mail > /dev/null 2>&1
+if [ $? = 0 ]; then
+	_err "mail database already exist"
+fi
+
 cd $DEFAULT/admin && env LD_PRELOAD=/usr/local/lib/ruby/gems/1.8/gems/mysql-2.9.1/lib/mysql/mysql_api.so rake -s db:setup RAIL_ENV=production
